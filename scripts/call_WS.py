a22fb753d1ac1dc4c8217118593ffbc25871c238
Charles Moulliard <cmoulliard@redhat.com>
2018-04-24 11:30:01 +0200

    Use correct id to get array info

--- scripts/call_WS.py
+++ scripts/call_WS.py
@@ -7,8 +7,8 @@ soap_request_file = sys.argv[2]
 url = webservice.split("/")
 
 request = open(soap_request_file,"r").read()
-webservice = httplib.HTTP("%s" % url[0])
-webservice.putrequest("POST", "/%s" % url[1])
+webservice = httplib.HTTP("%s" % url[2])
+webservice.putrequest("POST", "/%s" % url[3])
 webservice.putheader("Host", "localhost")
 webservice.putheader("User-Agent", "Python post")
 webservice.putheader("Content-type", "text/xml; charset=\"UTF-8\"")
